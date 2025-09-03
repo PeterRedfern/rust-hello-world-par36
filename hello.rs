316ea5a85c44f27de8e28b1d8e0304f0ec374bf1
PeterRedfern <145147143+PeterRedfern@users.noreply.github.com>
2025-09-03 12:14:36 -0400

    Add files via upload

---
+++ hello.rs
@@ -0,0 +1,9 @@
+// Peter Redfern - 9/3/25 Assignment 1
+fn main() {
+    // Statements here are executed when the compiled binary is called.
+
+    // Print text to the console.
+    println!("Hello World!");
+    println!("I'm a Rustacean!");
+    println!("Learning Rust is fun!")
+}
